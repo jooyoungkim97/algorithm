05935eccaacc10aa75014cca51e90bcc532b1abe
jooyoungkim97 <101385796+jooyoungkim97@users.noreply.github.com>
2022-05-11 17:17:50 +0900

    Create [DP]baekjoon(1463).py

---
+++ [DP]baekjoon(1463).py
@@ -0,0 +1,12 @@
+n = int(input())
+
+dp = [0]*(n+3)
+dp[2] = 1
+dp[3] = 1
+for i in range(4,n+1):
+    dp[i] = dp[i-1]+1
+    if i%2 == 0 :
+        dp[i] = min(dp[i//2]+1,dp[i])
+    if i%3 == 0:
+        dp[i] = min(dp[i],dp[i//3]+1)
+print(dp[n])
