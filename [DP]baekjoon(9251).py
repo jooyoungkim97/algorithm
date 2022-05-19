0bbb6445dd03aba4890ef55a9bbe09f391d03f1d
jooyoungkim97 <101385796+jooyoungkim97@users.noreply.github.com>
2022-05-19 12:14:12 +0900

    Create [DP]baekjoon(9251).py

---
+++ [DP]baekjoon(9251).py
@@ -0,0 +1,10 @@
+s1 = input()
+s2 = input()
+dp = [[0]*(len(s1)+1) for _ in range(len(s2)+1)]
+for i in range(1,len(s2)+1):
+    for j in range(1,len(s1)+1):
+        if s2[i-1]==s1[j-1]:
+            dp[i][j] = dp[i-1][j-1]+1
+        else:
+            dp[i][j] = max(dp[i][j-1],dp[i-1][j])
+print(dp[len(s2)][len(s1)])
